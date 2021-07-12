467ddf31846acc70b5458c6f88adc126f7f9f22d
Ivan Kostic <ivan.webdev5@yandex.ru>
2021-07-12 18:36:40 +0300

    test

--- src/modules/CreateNewCV/index.tsx
+++ src/modules/CreateNewCV/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Layout, Breadcrumb, Row, Col, Form } from "antd";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Layout, Breadcrumb, Row, Col, Form } from 'antd';
+import { Link } from 'react-router-dom';
 // import ProfileCP from 'components/forms/profileInfo/Index';
 // import ExperienceCP from 'components/forms/experience/Experience';
 // import ProjectCP from 'components/forms/Project';
@@ -14,17 +14,17 @@ const { Header, Content, Footer } = Layout;
 const CreateNewCV = () => (
   <div className="main">
     <Layout>
-      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
         <div className="logo" />
       </Header>
-      <Content style={{ padding: "0 50px", marginTop: 64 }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
+      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>
             <Link to="/">Dashboard</Link>
           </Breadcrumb.Item>
           <Breadcrumb.Item>CreateNew</Breadcrumb.Item>
         </Breadcrumb>
-        <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
+        <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
           <Row gutter={16}>
             <Col span={24}>
               <Form layout="vertical">
@@ -36,7 +36,7 @@ const CreateNewCV = () => (
           </Row>
         </div>
       </Content>
-      <Footer style={{ textAlign: "center" }}>CV Builder</Footer>
+      <Footer style={{ textAlign: 'center' }}>CV Builder</Footer>
     </Layout>
   </div>
 );
